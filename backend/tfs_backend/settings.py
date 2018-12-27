ba435fbdf8b69b042ed7c0eb595aea3bea79bb2d
TejasBhitle <bhitletejas@gmail.com>
2018-12-27 18:55:54 +0530

    Minor changes

--- backend/tfs_backend/settings.py
+++ backend/tfs_backend/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'People',
+    'projects',
+    
     # third party
     'rest_framework',
-    'projects',
+    
 
 ]
 
