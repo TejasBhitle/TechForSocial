ebd9b0baaaa0019c0577b046191e6a3c16f1a720
darshan761 <darshanpatil761@gmail.com>
2018-12-28 22:49:21 +0530

    Virtualenv added

--- backend/backend_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/tejas/102C1324102C1324/PROJECTS/TechForSocial/backend_new/backend_venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
