8c4648341c4ec43e46bb2100648122375ee16d55
TejasBhitle <bhitletejas@gmail.com>
2019-02-01 11:42:16 +0530

    Minor fixes

--- frontend/src/app/course/course.component.ts
+++ frontend/src/app/course/course.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FirebaseDbService } from '../firebase-db.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
   styleUrls: ['./course.component.scss']
 })
-export class CourseComponent implements OnInit {
+export class CourseComponent implements OnInit, OnDestroy {
 
   openProjects$ = []
+  showSpinner: boolean = false
+  subscription: Subscription
 
   constructor(private firebaseDb: FirebaseDbService) { }
 
   ngOnInit() {
-    this.firebaseDb.getProjects().subscribe( 
+    this.showSpinner = true
+    this.subscription = this.firebaseDb.getProjects().subscribe( 
       projects =>{
         this.openProjects$ = []
         projects.forEach( project => {
           if(project['isOpen'])
             this.openProjects$.push(project)
         })
+        this.showSpinner = false
+        this.subscription.unsubscribe()
       })
+  }
+
+  ngOnDestroy(){
+    if(this.subscription)
+      this.subscription.unsubscribe()
   }
 
 }
